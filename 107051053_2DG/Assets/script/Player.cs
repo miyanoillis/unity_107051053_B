a470243f487434edded90ac7e6fefc4391b63b8c
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-11-10 15:59:04 +0800

    complete

--- 107051053_2DG/Assets/script/Player.cs
+++ 107051053_2DG/Assets/script/Player.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject end;
+    public Text ciontx;
+    public int cion;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print(1);
-        end.SetActive(true);
-    }
+        if (collision.name == "Particle System Flh")
+        {
+            end.SetActive(true);
+            FindObjectOfType<KID.CharacterMovement2D>().enabled = false;
+        }
+        if(collision.tag == "cherry")
+        {
+            Destroy(collision.gameObject);
 
+            cion++;
+            ciontx.text = cion + "/999";
+        }
+
+    }
 }
---
+++ 107051053_2DG/Assets/script/level.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level : MonoBehaviour
+{
+    public void next(string Lv)
+    {
+        SceneManager.LoadScene(Lv);
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene("2DG_menu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
